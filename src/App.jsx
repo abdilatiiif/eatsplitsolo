5fecfb63da0ee66e3acf10baa0337b6cbbdf6841
Latif <latif_1995@live.no>
2025-01-21 19:27:29 +0100

    restart

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+/* eslint-disable react/prop-types */
+
+import React from "react";
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+function App() {
+  const [friends, setFriends] = React.useState(initialFriends);
+
+  return (
+    <div className="sidebar">
+      <FriendsList friends={friends} />
+    </div>
+  );
+}
+
+function FriendsList({ friends }) {
+  console.log(friends);
+  return (
+    <ul>
+      {friends.map((friend) => {
+        return <Friend friend={friend} key={friend.id} />;
+      })}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          {" "}
+          you owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {" "}
+          {friend.name} ows you ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && <p> You and {friend.name} are even! </p>}
+    </li>
+  );
+}
+
+export default App;
